566ea3268e1d06112cebe0651db9b5fb1ae81819
albertprofe <albertprofe@gmail.com>
2025-03-18 08:44:29 +0100

    feat: getAll users

--- userBorrowBookFront/src/Books.jsx
+++ userBorrowBookFront/src/Books.jsx
@@ -19,7 +19,7 @@ const Books = () => {
   // Fetch all books
   const fetchBooks = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/api/v1/books");
+      const response = await axios.get("/books");
       setBooks(response.data);
     } catch (error) {
       console.error("Error fetching books:", error);
--- userBorrowBookFront/src/Users.jsx
+++ userBorrowBookFront/src/Users.jsx
@@ -15,7 +15,7 @@ const Users = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get("/appusers");
+      const response = await axios.get("/users");
       setUsers(response.data);
     } catch (error) {
       console.error("Error fetching users:", error);
